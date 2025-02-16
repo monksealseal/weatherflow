289bb40129da2da8ceb577567ee8c0374d492653
Eduardo Siman <33988542+monksealseal@users.noreply.github.com>
2025-02-16 12:18:29 -0500

    Update __init__.py

--- weatherflow/models/__init__.py
+++ weatherflow/models/__init__.py
@@ -1,5 +1,21 @@
-
+# weatherflow/models/__init__.py
+from .base import BaseWeatherModel
+from .flow_matching import WeatherFlowMatch, ConvNextBlock, Swish, SinusoidalTimeEmbedding, TemporalAttention, VelocityFieldNet
 from .physics_guided import PhysicsGuidedAttention
 from .stochastic import StochasticFlowModel
-from .base import BaseWeatherModel
-from .flow_matching import WeatherFlowMatch
+from ..train import FlowVisualizer, ODESolver #Import here to make them availabe from weatherflow
+
+# __all__ is a good practice to explicitly define the public API
+__all__ = [
+    'BaseWeatherModel',
+    'WeatherFlowMatch',
+    'PhysicsGuidedAttention',
+    'StochasticFlowModel',
+    'ConvNextBlock',
+    'Swish',
+    'SinusoidalTimeEmbedding',
+    'TemporalAttention',
+    'VelocityFieldNet',
+    'FlowVisualizer',
+    'ODESolver'
+]
