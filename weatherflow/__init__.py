1f681c6426c89a25fed0383922e8856b8202ef9b
monksealseal <esiman@msn.com>
2025-02-16 03:12:44 +0000

    Fix: Update package structure for PyPI release

--- weatherflow/__init__.py
+++ weatherflow/__init__.py
@@ -1,1 +1,15 @@
-__version__ = "0.1.1"
+from .version import __version__
+
+# Import main classes
+from .data import WeatherDataset, ERA5Dataset
+from .models import PhysicsGuidedAttention, StochasticFlowModel
+from .utils import WeatherVisualizer
+
+__all__ = [
+    "WeatherDataset",
+    "ERA5Dataset",
+    "PhysicsGuidedAttention",
+    "StochasticFlowModel",
+    "WeatherVisualizer",
+    "__version__"
+]
---
+++ weatherflow/version.py
@@ -0,0 +1,2 @@
+# Version of the weatherflow package
+__version__ = "0.1.0"
