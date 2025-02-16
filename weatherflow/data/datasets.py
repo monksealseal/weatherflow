30efd3f74d458fb7b4f82ab834748c8718f9591e
monksealseal <esiman@msn.com>
2025-02-16 03:05:47 +0000

    Complete package setup with all necessary files

--- weatherflow/data/datasets.py
+++ weatherflow/data/datasets.py
@@ -9,19 +9,11 @@ class WeatherDataset:
     """Dataset class for loading weather data from HDF5 files."""
     
     def __init__(self, data_path: str, variables: List[str]):
-        """
-        Initialize WeatherDataset.
-        
-        Args:
-            data_path: Path to data directory
-            variables: List of variables to load (e.g., ["temperature"])
-        """
         self.data_path = Path(data_path)
         self.variables = variables
         self._load_data()
     
     def _load_data(self):
-        """Load data from HDF5 files."""
         self.data = {}
         for var in self.variables:
             file_path = self.data_path / f"{var}_train.h5"
@@ -47,40 +39,24 @@ class ERA5Dataset:
         variables: Optional[List[str]] = None,
         years: Optional[List[int]] = None
     ):
-        """
-        Initialize ERA5Dataset.
-        
-        Args:
-            data_path: Path to ERA5 data. Can be local path or GCS URL.
-                      If None, uses default WeatherBench 2 URL.
-            resolution: One of "64x32", "240x121", "1440x721"
-            variables: List of variables to load. If None, loads all.
-            years: List of years to load. If None, loads all.
-        """
         self.data_path = data_path or self.DEFAULT_URL
         self.variables = variables
         self.years = years
         self._load_data()
     
     def _load_data(self):
-        """Load ERA5 data from local or GCS path."""
         try:
-            # Handle both local and GCS paths
             if self.data_path.startswith("gs://"):
                 mapper = fsspec.get_mapper(self.data_path)
                 ds = xr.open_zarr(mapper)
             else:
                 ds = xr.open_zarr(self.data_path)
             
-            # Filter by variables if specified
             if self.variables:
                 ds = ds[self.variables]
             
-            # Filter by years if specified
             if self.years:
-                ds = ds.sel(
-                    time=ds.time.dt.year.isin(self.years)
-                )
+                ds = ds.sel(time=ds.time.dt.year.isin(self.years))
             
             self.data = ds
             
@@ -94,8 +70,6 @@ class ERA5Dataset:
     def __getitem__(self, idx: int) -> Tuple[np.ndarray, np.ndarray]:
         if self.data is None:
             raise ValueError("No data loaded")
-            
         current = self.data.isel(time=idx)
         next_state = self.data.isel(time=idx + 1)
-        
         return current.to_array().values, next_state.to_array().values
